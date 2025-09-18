82ff4f76a0218ff6be72069eb41d25f41c346cca
tran-ethan <tran.ethan@hotmail.com>
2025-09-18 13:26:58 -0400

    Restyle Navbar

--- src/components/Nav/IconContainer.jsx
+++ src/components/Nav/IconContainer.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 export const IconContainer = styled.div`
   cursor: pointer;
-  margin: 8px 0 0 34px;
-  margin: 8px 0 0 calc(1.65vw - 7px); // NEW
+  // margin: 8px 0 0 34px;
+  margin: 8px 0 0 calc(1.7vw); // NEW
 `
 
 export default IconContainer
