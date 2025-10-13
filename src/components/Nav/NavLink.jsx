61f87bd0aaef4e764b509ffba8ee4b6ceb0185cf
tran-ethan <tran.ethan@hotmail.com>
2025-10-13 15:44:42 -0400

    Fix nav link text style

--- src/components/Nav/NavLink.jsx
+++ src/components/Nav/NavLink.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components"
 import * as styleVars from "../variable"
 
 export const NavLink = styled.a`
-  font-family: 'Alfacade', sans-serif;
-  font-size: 20px;
+  font-family: 'Afacad', sans-serif;
+  font-size: 25px;
   font-weight: 500;
   display: flex;
   cursor: pointer;
@@ -20,15 +20,15 @@ export const NavLink = styled.a`
   }
 
   .home-link {
-  font-family: 'Alfacade';
-  font-size: 20px;
-  font-weight: 500;
-  display: flex;
-  cursor: pointer;
-  color: ${styleVars.m13Red};
-  text-decoration: none;
-  transition: color 0.3s ease;}
-}
+    font-family: 'Afacad', sans-serif;
+    font-size: 20px;
+    font-weight: 500;
+    display: flex;
+    cursor: pointer;
+    color: ${styleVars.m13Red};
+    text-decoration: none;
+    transition: color 0.3s ease;}
+  }
 `
 
 export default NavLink
