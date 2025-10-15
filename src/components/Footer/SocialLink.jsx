7e4c76bcf1b39428b2e4aad6c46b7a770f78b7b6
Mubeen Mohammed <mubeen.mohammed@mail.mcgill.ca>
2025-10-15 00:54:20 -0400

    updated styles for footer

--- src/components/Footer/SocialLink.jsx
+++ src/components/Footer/SocialLink.jsx
@@ -1,15 +1,18 @@
 import styled from "styled-components"
 import * as styleVars from "../variable"
 
+const currentPath = window.location.pathname;
+const isSpecialPage = !["/privacy/", "/code-of-conduct/", "/discord-guide/"].includes(currentPath);
+
 export const SocialLink = styled.a`
-  color: ${styleVars.hackBlack60};
+  color: ${isSpecialPage ? styleVars.hackWhite : styleVars.m13Red};
   cursor: pointer;
   text-decoration: none;
   font-size: 16px;
 
   &:focus,
   &:hover {
-    color: ${styleVars.colorHackRed};
+    color: ${isSpecialPage ? styleVars.colorHackRed : styleVars.m12Maroon};
   }
 `
 
