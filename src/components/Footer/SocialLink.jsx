ee4cdd9dddd725847f2fbf817c862652da841857
Mubeen Mohammed <mubeen.mohammed@mail.mcgill.ca>
2025-10-17 01:09:46 -0400

    fixed build errors

--- src/components/Footer/SocialLink.jsx
+++ src/components/Footer/SocialLink.jsx
@@ -1,19 +1,24 @@
 import styled from "styled-components"
 import * as styleVars from "../variable"
 
-const currentPath = window.location.pathname;
-const isSpecialPage = !["/privacy/", "/code-of-conduct/", "/discord-guide/"].includes(currentPath);
-
 export const SocialLink = styled.a`
-  color: ${isSpecialPage ? styleVars.hackWhite : styleVars.m13Red};
+  color: ${() =>
+    typeof window !== "undefined" &&
+    !["/privacy/", "/code-of-conduct/", "/discord-guide/"].includes(window.location.pathname)
+      ? styleVars.hackWhite
+      : styleVars.m13Red};
   cursor: pointer;
   text-decoration: none;
   font-size: 16px;
 
   &:focus,
   &:hover {
-    color: ${isSpecialPage ? styleVars.colorHackRed : styleVars.m12Maroon};
+    color: ${() =>
+      typeof window !== "undefined" &&
+      !["/privacy/", "/code-of-conduct/", "/discord-guide/"].includes(window.location.pathname)
+        ? styleVars.colorHackRed
+        : styleVars.m12Maroon};
   }
-`
+`;
 
-export default SocialLink
+export default SocialLink;
