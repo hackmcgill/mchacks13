a01ab9e586c08ba11c5c06decf3d4a0e69dc0272
tran-ethan <tran.ethan@hotmail.com>
2025-10-23 17:30:09 -0400

    fix navbar menu and add home logo in mobile view

---
+++ src/components/Nav/HomeLogoContainer.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import * as styleVars from "../variable"
+
+export const HomeLogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  padding: 1rem 0;
+  top: 14px;
+  left: calc(40px);
+  gap: 2.5vw;
+  height: 60px;
+
+  @media only screen and (min-width: ${styleVars.smUp}) {
+    display: none;
+  }
+`
+
+export default HomeLogoContainer
