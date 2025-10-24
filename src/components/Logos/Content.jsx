c3d377f2f4512a17341ff2ef89a8ff165e891ad3
Mubeen Mohammed <mubeen.mohammed@mail.mcgill.ca>
2025-10-23 22:45:36 -0400

    changed the past sponsors hearder to new design

--- src/components/Logos/Content.jsx
+++ src/components/Logos/Content.jsx
@@ -8,11 +8,25 @@ export const Content = styled.section`
   gap: 10px;
 
   h1 {
-    font-size: 6rem;
+    font-size: 40px;
     text-align: center;
-    color: ${styleVars.m12DarkestTeal};
+    color: ${styleVars.m13White};
     margin: 7rem 0 2rem 0;
-    font-family: "ArcadeClassic", sans-serif;
+    font-family: "Averia Serif Libre";
+    font-style: italic;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -2px;
+  }
+
+  @media only screen and (max-width: ${styleVars.xsUp}) {
+    h1 {
+      font-size: 17px;
+      font-style: italic;
+      font-weight: 700;
+      line-height: normal;
+      letter-spacing: -0.85px;
+    }
   }
 `
 
