622688dc0a31c1e746b6b439f4e2ca5c4cbf4ae7
tran-ethan <tran.ethan@hotmail.com>
2025-10-26 21:24:46 -0400

    switch favicon icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/assets/images/favicon.png`,
+        icon: `src/assets/images/favicon.svg`,
       },
     },
     {
