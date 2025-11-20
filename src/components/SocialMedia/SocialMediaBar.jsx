e32e18a51333d3884ecc79f89a9371674715325d
tran-ethan <tran.ethan@hotmail.com>
2025-11-20 14:18:34 -0500

    changed new cursor hover image and fixed cursor crashing out on icon links

--- src/components/SocialMedia/SocialMediaBar.jsx
+++ src/components/SocialMedia/SocialMediaBar.jsx
@@ -8,6 +8,7 @@ import { ReactComponent as GitHub } from "../../assets/images/logos/mchacks13-gi
 import { ReactComponent as Instagram } from "../../assets/images/logos/mchacks13-ig-logo.svg"
 import { ReactComponent as Twitter } from "../../assets/images/logos/mchacks13-x-logo.svg"
 import { ReactComponent as LinkedIn } from "../../assets/images/logos/mchack13-linkedin.svg"
+import NavLink from "../Nav/NavLink"
 
 const SocialMediaBar = () => (
   <Container>
@@ -17,50 +18,50 @@ const SocialMediaBar = () => (
       </a>
     </div> */}
     <div className="SocialMediaBar__icon">
-      <a href="https://www.linkedin.com/company/mchacks/" target="_blank" rel="noopener noreferrer">
+      <NavLink href="https://www.linkedin.com/company/mchacks/" target="_blank" rel="noopener noreferrer">
         <LinkedIn className="icon"/>
-      </a>
+      </NavLink>
     </div>
     <div className="SocialMediaBar__icon">
-      <a
+      <NavLink
         href="https://www.facebook.com/mcgillhacks/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Facebook className="icon"/>
-      </a>
+      </NavLink>
     </div>
     <div className="SocialMediaBar__icon">
-      <a
+      <NavLink
         href="https://www.instagram.com/mcgillhacks/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Instagram className="icon"/>
-      </a>
+      </NavLink>
     </div>
     <div className="SocialMediaBar__icon">
-      <a
+      <NavLink
         href="https://github.com/hackmcgill"
         target="_blank"
         rel="noopener noreferrer"
       >
         <GitHub className="icon"/>
-      </a>
+      </NavLink>
     </div>
     <div className="SocialMediaBar__icon">
-      <a
+      <NavLink
         href="https://twitter.com/McGillHacks?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Twitter className="icon"/>
-      </a>
+      </NavLink>
     </div>
     <div className="SocialMediaBar__icon">
-      <a href="https://mchacks-12.devpost.com/" target="_blank" rel="noopener noreferrer">
+      <NavLink href="https://mchacks-13.devpost.com/" target="_blank" rel="noopener noreferrer">
         <Devpost className="icon"/>
-      </a>
+      </NavLink>
     </div>
   </Container>
 )
