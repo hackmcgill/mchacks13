9942621b96f20a754d28702f6d3189304961f408
tran-ethan <tran.ethan@hotmail.com>
2026-01-07 20:41:10 -0500

    fix text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -58,9 +58,9 @@ const Hero = () => (
       <div className="grid__clouds"></div>
       <p className="hero__challenge-text">
         Want to win $1000? Sign up for the Backboard.io x McHacks 13
-        Pre-Hackathon Challenge submit your project by January 14th, 11:59pm
+        Pre-Hackathon Challenge and submit your project by January 14th, 11:59pm
         PST! Open to anyone and everyone. You can still participate even if you
-        are not a McHacks 13 participant.
+        are not a McHacks 13 participant. <b>Click the portal below for more info!</b>
       </p>
       <div className="invisible-apply" onClick={apply} />
       <img
